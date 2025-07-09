3a705510e2404ea551500b1cd6b561d0c6e632ee
Monisha RK <monisha.rk2415@gmail.com>
2025-07-09 20:57:29 +0200

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -15,10 +15,10 @@ import torch
 
 def preprocess_image(image):
     transform = transforms.Compose([
-        transforms.Grayscale(),                       # MNIST dataset: grayscale
+        transforms.Grayscale(),                       # MNIST dataset: Grayscale
         transforms.Resize((28, 28)),                  # MNIST dataset: 28x28
         transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))    # MNIST dataset: mean and std values
+        transforms.Normalize((0.1307,), (0.3081,))    # MNIST dataset: Mean and std values
     ])
     image = transform(image).unsqueeze(0)             # Adds batch dim -> 1x1x28x28
     return image
